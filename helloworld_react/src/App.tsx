bf4d7623436338b9b0c11b33265466dccee1bcdb
Júlio Pereira Machado <julioapm@gmail.com>
2021-12-28 15:15:51 -0300

    Componente funcional

---
+++ helloworld_react/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import HelloFunctionComponent from './components/helloFunctionComponent';
+
+function App() {
+  return (
+    <HelloFunctionComponent name="Júlio Machado" />
+  );
+}
+
+export default App;
