bd65eca69f848694d5cd7e1a7b2afd6d80a6402e
Júlio Pereira Machado <julioapm@gmail.com>
2021-12-28 17:26:42 -0300

    Counter funcional

---
+++ counters_react/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import CounterFunction from './components/CounterFunction';
+
+function App() {
+  return (
+    <>
+      <CounterFunction />
+      <CounterFunction />
+    </>
+  );
+}
+
+export default App;
---
+++ counters_react/src/components/CounterFunction.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function CounterFunction() {
+   const [count, setCount] = useState(0);
+
+   const handleClick = () => {
+       setCount(count + 1);
+       //setCount(currentCount => currentCount + 1);
+   };
+
+   return (
+       <div>
+           <p>You clicked {count} times</p>
+           <button onClick={handleClick}>Click Me</button>
+       </div>
+   );
+}
+
+export default CounterFunction;
